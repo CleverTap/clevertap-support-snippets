0c0b479ad1ec5e3b25c36e79a83f355cfffba516
Anand Jain <helloanand@gmail.com>
2016-05-19 12:31:51 +0530

    Update Helloworld.java

--- Helloworld.java
+++ Helloworld.java
@@ -1,6 +1,6 @@
 HashMap<String, Object> prodViewedAction = new HashMap<String, Object>();
-prodViewedAction.put("Product Name", "Casio Chronograph Watch");
-prodViewedAction.put("Category", "Mens Accessories");
-prodViewedAction.put("Price", 59.99);
-prodViewedAction.put("Currency", "USD");
-cleverTap.event.push("Product viewed", prodViewedAction);
+  prodViewedAction.put("Product Name", "Casio Chronograph Watch");
+  prodViewedAction.put("Category", "Mens Accessories");
+  prodViewedAction.put("Price", 59.99);
+  prodViewedAction.put("Currency", "USD");
+  cleverTap.event.push("Product viewed", prodViewedAction);
