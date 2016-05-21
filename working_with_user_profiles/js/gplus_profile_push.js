9d644107a77a7e08e8cd17c77447023c90bac391
Anand Jain <helloanand@gmail.com>
2016-05-21 18:27:15 +0530

    Create gplus_profile_push.js

---
+++ working_with_user_profiles/js/gplus_profile_push.js
@@ -0,0 +1,11 @@
+gapi.client.plus.people.get({
+    'userId': 'me'}).execute(function(resp) {
+    clevertap.profile.push({"Google Plus":resp});
+});
+
+/**
+Recommended scopes are:
+- plus.login
+- plus.me
+- E-mail
+*/
