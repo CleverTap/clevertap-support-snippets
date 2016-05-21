dce3e6564294c08ef8cb0e60d3c2bbb81fe03f9a
helloanand <helloanand@gmail.com>
2016-05-21 20:00:26 +0530

    Example of updating basic user profile

---
+++ working_with_user_profiles/ios/update_profile_basic.m
@@ -0,0 +1,17 @@
+NSDateComponents *dob = [[NSDateComponents alloc] init];
+dob.day = 24;
+dob.month = 5;
+dob.year = 1992;
+NSDate *d = [[NSCalendar currentCalendar] dateFromComponents:dob];
+NSDictionary *profile = @{@"Name" : @"Jack Montana", // String
+@"Identity" : @6541182, // String or number
+@"Email" : @"jack@gmail.com", // Email address of the user
+@"Phone" : @4155551234, // Phone (exclude the country code)
+@"Gender" : @"M", // Can be either M or F
+@"Employed" : @"Y", // Can be either Y or N
+@"DOB" : d, // Date of Birth. An NSDate object
+@"Age" : @28, // Not required if DOB is set
+@"MSG-email" : @NO, // Disable email notifications
+@"MSG-push" : @YES, // Enable push notifications
+@"MSG-sms" : @NO}; // Disable SMS notifications
+[[CleverTap sharedInstance] profilePush:profile];
---
+++ working_with_user_profiles/js/update_profile_basic.js
@@ -0,0 +1,17 @@
+clevertap.profile.push({
+ "Site": {
+   "Name": "Jack Montana", // User's name
+   "Identity": 6541182, // User's identity
+   "Email": "jack@gmail.com", // User's email
+   "Phone": "+14155551234", // User's phone with country code
+   "Gender": "M", // Can be either M or F
+   "Employed": "Y", // Can be either Y or N
+   "Education": "Graduate", // Can be either School, College or Graduate
+   "Married": "Y", // Can be either Y or N
+   "DOB": $WZRK_WR.setDate(20150501), // Date of Birth. Format date as yyyymmdd
+   "Age": 67, // Not required if DOB is set
+   "MSG-email": false, // Disable email notifications
+   "MSG-push": true, // Enable push notifications
+   "MSG-sms": true // Enable sms notifications
+ }
+});
