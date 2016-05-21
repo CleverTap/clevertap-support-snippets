b87c5a61396ea4753c550271a4002256f3c06b8d
Anand Jain <helloanand@gmail.com>
2016-05-21 15:30:49 +0530

    Removed Currency from example

--- Helloworld.java
+++ Helloworld.java
@@ -2,5 +2,4 @@ HashMap<String, Object> prodViewedAction = new HashMap<String, Object>();
   prodViewedAction.put("Product Name", "Casio Chronograph Watch");
   prodViewedAction.put("Category", "Mens Accessories");
   prodViewedAction.put("Price", 59.99);
-  prodViewedAction.put("Currency", "USD");
   cleverTap.event.push("Product viewed", prodViewedAction);
