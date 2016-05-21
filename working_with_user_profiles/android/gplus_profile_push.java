a6c42424f228ed2b95b9be2d9273fe9a2094c0c8
Anand Jain <helloanand@gmail.com>
2016-05-21 18:28:55 +0530

    Added recommended scopes

--- working_with_user_profiles/android/gplus_profile_push.java
+++ working_with_user_profiles/android/gplus_profile_push.java
@@ -3,3 +3,7 @@ public void onConnected(Bundle bundle) {
   Person currentPerson = Plus.PeopleApi.getCurrentPerson(mGoogleApiClient);
   cleverTap.profile.pushGooglePlusPerson(currentPerson);
 }
+
+//Recommended scopes for Google Plus are Plus.SCOPE_PLUS_LOGIN, and Plus.SCOPE_PLUS_PROFILE.
+
+
