ff32f1156572b0dd48ee1380784d542eeeecb7fd
Anand Jain <helloanand@gmail.com>
2016-05-21 18:29:38 +0530

    Added recommended scopes

--- working_with_user_profiles/ios/gplus_profile_push.m
+++ working_with_user_profiles/ios/gplus_profile_push.m
@@ -1,3 +1,6 @@
 GPPSignIn *signIn = [GPPSignIn sharedInstance];
   GTLPlusPerson *p = signIn.googlePlusUser;
   [[CleverTap sharedInstance] profilePushGooglePlusUser:p];
+
+// Recommended scopes are kGTLAuthScopePlusMe, kGTLAuthScopePlusLogin, kGTLAuthScopePlusUserinfoEmail, and kGTLAuthScopePlusUserinfoProfile.
+
