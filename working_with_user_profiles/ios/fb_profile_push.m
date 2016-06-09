99c4cc858e7f0f7740919e25ca034636041bdab3
Jude Pereira <judebpereira@gmail.com>
2016-06-09 17:32:05 +0530

    Update fb_profile_push.m

--- working_with_user_profiles/ios/fb_profile_push.m
+++ working_with_user_profiles/ios/fb_profile_push.m
@@ -1,7 +1,7 @@
 [[[FBSDKGraphRequest alloc] initWithGraphPath:@"me?fields=id,name,email,birthday,gender,education,work"
                                    parameters:nil]
  startWithCompletionHandler:^(FBSDKGraphRequestConnection *connection, id result, NSError *error) {
-     if (result) {
+     if (!error) {
          [[CleverTap push] graphUser:result];
      }
  }];
