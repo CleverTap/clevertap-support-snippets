0d824dbe38610123723dff5d72af9aa07c96d489
Jude Pereira <judebpereira@gmail.com>
2016-06-09 17:17:57 +0530

    Update fb_profile_push.java

--- working_with_user_profiles/android/fb_profile_push.java
+++ working_with_user_profiles/android/fb_profile_push.java
@@ -1,1 +1,8 @@
-cleverTap.profile.pushFacebookUser(jsonObject);
+GraphRequest.newMeRequest(accessToken, new GraphRequest.GraphJSONObjectCallback() {
+    @Override
+    public void onCompleted(JSONObject user, GraphResponse response) {
+        if (user != null) {
+            cleverTap.profile.pushFacebookUser(user);
+        }
+    }
+}).executeAsync();
