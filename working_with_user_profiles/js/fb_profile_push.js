1238bb59c8a13afe79530a5df82c180819d7d8f8
Jude Pereira <judebpereira@gmail.com>
2016-06-09 17:35:23 +0530

    Update fb_profile_push.js

--- working_with_user_profiles/js/fb_profile_push.js
+++ working_with_user_profiles/js/fb_profile_push.js
@@ -1,10 +1,3 @@
-FB.api('/me', function(userData) {
-   clevertap.profile.push({"Facebook": userData});
+FB.api('/me', {fields: 'id,name,email,birthday,gender,education,work'}, function(userData) {
+    clevertap.profile.push({"Facebook": userData});
 });
-
-/**
- * A call with additional permissions, for example - email, would look like this:
- * FB.api('/me', {fields: 'email'}, function(userData) {
- *  clevertap.profile.push({"Facebook": userData});
- * });
- */
