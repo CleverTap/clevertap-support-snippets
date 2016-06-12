716d4b2554c637a20143f24ed500db6f5a3b993d
Jude Pereira <judebpereira@gmail.com>
2016-06-13 01:34:30 +0530

    Cleanup

--- working_with_user_profiles/ios/gplus_profile_push.m
+++ working_with_user_profiles/ios/gplus_profile_push.m
@@ -1,5 +1,5 @@
 GPPSignIn *signIn = [GPPSignIn sharedInstance];
-  GTLPlusPerson *p = signIn.googlePlusUser;
-  [[CleverTap sharedInstance] profilePushGooglePlusUser:p];
+GTLPlusPerson *p = signIn.googlePlusUser;
+[[CleverTap sharedInstance] profilePushGooglePlusUser:p];
 
 // Recommended scopes for Google Plus are kGTLAuthScopePlusMe, kGTLAuthScopePlusLogin, kGTLAuthScopePlusUserinfoEmail, and kGTLAuthScopePlusUserinfoProfile
--- working_with_user_profiles/ios/update_profile_basic.m
+++ working_with_user_profiles/ios/update_profile_basic.m
@@ -3,15 +3,18 @@ dob.day = 24;
 dob.month = 5;
 dob.year = 1992;
 NSDate *d = [[NSCalendar currentCalendar] dateFromComponents:dob];
-NSDictionary *profile = @{@"Name" : @"Jack Montana", // String
-@"Identity" : @6541182, // String or number
-@"Email" : @"jack@gmail.com", // Email address of the user
-@"Phone" : @4155551234, // Phone (exclude the country code)
-@"Gender" : @"M", // Can be either M or F
-@"Employed" : @"Y", // Can be either Y or N
-@"DOB" : d, // Date of Birth. An NSDate object
-@"Age" : @28, // Not required if DOB is set
-@"MSG-email" : @NO, // Disable email notifications
-@"MSG-push" : @YES, // Enable push notifications
-@"MSG-sms" : @NO}; // Disable SMS notifications
+NSDictionary *profile = @{
+    @"Name": @"Jack Montana",       // String
+    @"Identity": @6541182,          // String or number
+    @"Email": @"jack@gmail.com",    // Email address of the user
+    @"Phone": @4155551234,          // Phone (exclude the country code)
+    @"Gender": @"M",                // Can be either M or F
+    @"Employed": @"Y",              // Can be either Y or N
+    @"DOB": d,                      // Date of Birth. An NSDate object
+    @"Age": @28,                    // Not required if DOB is set
+    @"MSG-email": @NO,              // Disable email notifications
+    @"MSG-push": @YES,              // Enable push notifications
+    @"MSG-sms": @NO                 // Disable SMS notifications
+};
+
 [[CleverTap sharedInstance] profilePush:profile];
