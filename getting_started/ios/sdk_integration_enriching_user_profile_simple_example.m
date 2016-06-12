707b55786df33eacc7e5a7c92061a00f8da0c891
Jude Pereira <judebpereira@gmail.com>
2016-06-12 15:46:15 +0530

    Update sdk_integration_enriching_user_profile_simple_example.m

--- getting_started/ios/sdk_integration_enriching_user_profile_simple_example.m
+++ getting_started/ios/sdk_integration_enriching_user_profile_simple_example.m
@@ -1,5 +1,7 @@
 // Simple user profile enrichment example - adding name and an age to the user’s profile:
 
-NSDictionary *profile = @{@"Name" : @"Jack Montana", // String
-@"Age" : @28};
+NSDictionary *profile = @{
+    @"Name" : @"Jack Montana",
+    @"Age" : @28
+};
 [[CleverTap sharedInstance] profilePush:profile];
