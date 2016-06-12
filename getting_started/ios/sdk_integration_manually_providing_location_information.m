b2370adbb1580007328834d0780045324be7cd1e
Jude Pereira <judebpereira@gmail.com>
2016-06-12 15:45:03 +0530

    Update sdk_integration_manually_providing_location_information.m

--- getting_started/ios/sdk_integration_manually_providing_location_information.m
+++ getting_started/ios/sdk_integration_manually_providing_location_information.m
@@ -1,5 +1,5 @@
-// Manually providing Location information to CleverTap
-
-- (void)locationManager:(CLLocationManager ​*)manager didUpdateToLocation:(CLLocation *​)newLocation fromLocation:(CLLocation *)oldLocation {
-   [CleverTap setLocation: newLocation.coordinate];
+- (void)locationManager:(CLLocationManager *)manager 
+    didUpdateToLocation:(CLLocation *)newLocation 
+           fromLocation:(CLLocation *)oldLocation {
+    [CleverTap setLocation: newLocation.coordinate];
 }
