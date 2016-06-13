09fceb98a590cce9a05490188fab5dc4e404a984
sureshkon <suresh@clevertap.com>
2016-06-13 13:21:40 +0530

    typo. root is not defined. CleverTapInstance instead

--- getting_started/silverlight_80/clevertap_config_instance_code.cs
+++ getting_started/silverlight_80/clevertap_config_instance_code.cs
@@ -1,7 +1,7 @@
 RootFrame = new PhoneApplicationFrame();
 CleverTapConfig config = /*Initialize CleverTapConfig by one of the above methods*/
 CleverTapApi CleverTapInstance = CleverTapApi.Init(config);
-RootFrame.Navigated += root.Navigated;
+RootFrame.Navigated += CleverTapInstance.Navigated;
 
 // You can save the above instance as a static variable in App.xaml.cs, for it to be
 // accessed throughout the application.
