d106eb09e30b63eca3d8f2e294b864bcbf6b3d6f
Pranay Warke <pranaywarke@gmail.com>
2016-06-13 16:12:47 +0530

    Merging Silverlight 8.0 and 8.1

--- getting_started/silverlight_80/initialize_via_code.cs
+++ getting_started/silverlight_80/initialize_via_code.cs
@@ -6,10 +6,11 @@ config.RegisterForToastNotifications = true;
 /* Enable / Disable InApp Notifications from WizRocket [Default-true]*/
 config.EnableInAppNotifications = true;
 
-/* If Push Enabled, provide MPNS Channel Name [Default-WizRocket] */
+/* If Push Enabled, provide MPNS Channel Name [Default-WizRocket]. Channel Name not required for WNS */
 config.ChannelName = "WizRocket";
 
-/* If Authenticated Push Enabled, provide Service Name, Mandatory for Authenticated push, Do not initalize for unauthenticated push*/
+/* If Authenticated Push Enabled, provide Service Name, Mandatory for Authenticated push, Do not initalize for unauthenticated push.
+  Service Name not required for WNS*/
 config.ServiceName = "ServiceName";
 
 /* Enable / Disable personalization API [Default-false]*/
