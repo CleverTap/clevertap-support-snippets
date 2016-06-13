ce266751383ac382530ae41769a64e9ddd074728
Jude Pereira <judebpereira@gmail.com>
2016-06-13 12:50:57 +0530

    Update access_profile_data.swift

--- personalization/ios/access_profile_data.swift
+++ personalization/ios/access_profile_data.swift
@@ -4,4 +4,4 @@ let customerType = CleverTap.sharedInstance()?.profileGet("Customer Type")
 
 // To access a multi-value user profile property:
 // If the property is not available, this call will return nil
-let myStuff = (CleverTap.sharedInstance()?.profileGet("myStuff"))! as! Array<AnyObject>
+let myStuff = (CleverTap.sharedInstance()?.profileGet("myStuff")) as! Array<AnyObject>
