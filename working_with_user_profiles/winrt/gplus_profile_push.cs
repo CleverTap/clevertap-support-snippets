580894f8bc8cc7f8c9a1fb69d03c1fd121457314
Pranay Warke <pranaywarke@gmail.com>
2016-06-14 17:24:06 +0530

    Update gplus_profile_push.cs

--- working_with_user_profiles/winrt/gplus_profile_push.cs
+++ working_with_user_profiles/winrt/gplus_profile_push.cs
@@ -1,3 +1,3 @@
-CleverTapConfig.Profile.PushGooglePlusPerson(googlePlusUserJsonString);
+CleverTapInstance.Profile.PushGooglePlusPerson(googlePlusUserJsonString);
 
 // Recommended scopes for Google Plus are Plus.SCOPE_PLUS_LOGIN, and Plus.SCOPE_PLUS_PROFILE
