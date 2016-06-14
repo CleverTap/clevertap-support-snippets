bfa7160956d57548f2204b359a60caf7f31fca19
Pranay Warke <pranaywarke@gmail.com>
2016-06-14 18:05:50 +0530

    Update access_event_data.cs

--- personalization/winrt/access_event_data.cs
+++ personalization/winrt/access_event_data.cs
@@ -1,8 +1,8 @@
 // First time the event occurred
-CleverTapInstance.Event.GetFirstTime(eventName);
+CleverTapInstance.Event.GetFirstTime("Charged");
 
 //Last time the event occurred
-CleverTapInstance.Event.GetLastTime(eventName);
+CleverTapInstance.Event.GetLastTime("Charged");
 
 //Number of times event occurred previously
-CleverTapInstance.Event.GetOccurences(eventName);
+CleverTapInstance.Event.GetOccurences("Charged");
