2aaf58f89ece34d33a8fdb437822beecb71cd886
helloanand <helloanand@gmail.com>
2016-06-14 18:24:59 +0530

    personalization comment for winrt

--- personalization/winrt/access_event_data.cs
+++ personalization/winrt/access_event_data.cs
@@ -1,8 +1,8 @@
 // First time the event occurred
 CleverTapInstance.Event.GetFirstTime("Charged");
 
-//Last time the event occurred
+// Last time the event occurred
 CleverTapInstance.Event.GetLastTime("Charged");
 
-//Number of times event occurred previously
+// Number of times event occurred previously
 CleverTapInstance.Event.GetOccurences("Charged");
