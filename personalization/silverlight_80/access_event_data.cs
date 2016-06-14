9228ec3ba8ef391b5dc7df19a9eb318a973fc4a7
Pranay Warke <pranaywarke@gmail.com>
2016-06-14 18:05:08 +0530

    Update access_event_data.cs

--- personalization/silverlight_80/access_event_data.cs
+++ personalization/silverlight_80/access_event_data.cs
@@ -1,8 +1,8 @@
 // First time the event occurred
-CleverTapInstance.Event.GetFirstTime(string eventName);
+CleverTapInstance.Event.GetFirstTime("Charged");
 
 // Last time the event occurred
-CleverTapInstance.Event.GetLastTime(string eventName);
+CleverTapInstance.Event.GetLastTime("Charged");
 
 // Number of times event occurred previously
-CleverTapInstance.Event.GetOccurences(string eventName);
+CleverTapInstance.Event.GetOccurences("Charged");
