b7ad137c34fa67c09e25a4bd4b8878bbe2bf4348
Francis Pereira <francis@wizrocket.com>
2016-06-14 18:46:40 +0530

    Deleted SilverLight 8.1 code snippets

--- personalization/silverlight_81/access_profile_data.cs
+++
@@ -1,3 +0,0 @@
-// Get value of a profile property
-CleverTapApi CleverTapInstance = CleverTapApi.GetInstance();
-CleverTapInstance.Profile.GetProperty(string propertyName);
--- working_with_user_profiles/silverlight_81/gplus_profile_push.cs
+++
@@ -1,3 +0,0 @@
-CleverTapInstance.Profile.PushGooglePlusPerson(googlePlusUserJsonString);
-
-// Recommended scopes for Google Plus are Plus.SCOPE_PLUS_LOGIN, and Plus.SCOPE_PLUS_PROFILE
