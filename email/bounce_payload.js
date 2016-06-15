79de81592d686f67d8a230dc927398d680179799
helloanand <helloanand@gmail.com>
2016-06-15 08:49:09 +0530

    email bounce JSON payload

---
+++ email/bounce_payload.js
@@ -0,0 +1,24 @@
+[
+    {
+        "event": "softbounce",
+        "data": [
+            {
+                "e": "email1@emailprovider.com",  // email id that bounced
+                "ts": 1435322805                  // time of the bounce
+            },
+            {
+                "e": "email2@emailprovider2.com", // email id that bounced
+                "ts": 1435322805                  // time of the bounce
+            }
+        ]
+    },
+    {
+        "event": "hardbounce",
+        "data": [
+            {
+                "e": "email3@emailprovider.com",
+                "ts": 1435322805
+            }
+        ]
+    }
+]
