ae09df648c2acf0486d0ed1395221557a2d44674
ankit <ankit.j.arora@gmail.com>
2016-08-24 14:19:53 +0530

    pass an object instead of an array to notifications array

--- getting_started/js/push_sample_popup.js
+++ getting_started/js/push_sample_popup.js
@@ -1,6 +1,6 @@
-clevertap.notifications.push(
-    'Would you like to receive Push Notifications?',
-    'We promise to only send you relevant content and give you updates on your transactions',
-    'Sign me up!',
-    'No thanks',
-    '#f28046');
+clevertap.notifications.push({
+    "titleText":'Would you like to receive Push Notifications?',
+    "bodyText":'We promise to only send you relevant content and give you updates on your transactions',
+    "okButtonText":'Sign me up!',
+    "rejectButtonText":'No thanks',
+    "okButtonColor":'#f28046'});
