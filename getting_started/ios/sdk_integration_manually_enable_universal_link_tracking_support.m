fc894e0d97bd066fc6d43c4d383b40532dca5ee9
Peter <peter@pwang.co>
2016-09-23 16:55:57 -0700

    update iOS openURL

--- getting_started/ios/sdk_integration_manually_enable_universal_link_tracking_support.m
+++ getting_started/ios/sdk_integration_manually_enable_universal_link_tracking_support.m
@@ -5,3 +5,18 @@
       [[CleverTap sharedInstance] handleOpenURL:url sourceApplication:sourceApplication];
       return YES;
 }
+
+- (BOOL)application:(UIApplication *)app
+            openURL:(NSURL *)url
+            options:(NSDictionary *)options {
+    [[CleverTap sharedInstance] handleOpenURL:url sourceApplication:nil];
+    return YES;
+}
+
+- (void)openURL:(NSURL*)url options:(NSDictionary<NSString *, id> *)options
+completionHandler:(void (^ __nullable)(BOOL success))completion {
+    [[CleverTap sharedInstance] handleOpenURL:url sourceApplication:nil];
+    if (completion) {
+        completion(YES);
+    }
+}
--- getting_started/ios/sdk_integration_manually_enable_universal_link_tracking_support.swift
+++ getting_started/ios/sdk_integration_manually_enable_universal_link_tracking_support.swift
@@ -3,3 +3,15 @@ func application(application: UIApplication, openURL url: NSURL,
     CleverTap.sharedInstance()?.handleOpenURL(url, sourceApplication: sourceApplication)
     return true
 }
+
+// Swift 3
+func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
+   CleverTap.sharedInstance()?.handleOpen(url, sourceApplication: nil)
+   return true
+}
+
+func open(_ url: URL, options: [String : Any] = [:],
+                   completionHandler completion: ((Bool) -> Swift.Void)? = nil) {
+	CleverTap.sharedInstance()?.handleOpen(url, sourceApplication: nil)
+	completion?(false)
+}
