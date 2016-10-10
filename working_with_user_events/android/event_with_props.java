47730bb1455a984a97dd927da96bc40c949386bf
Jay Dalvi <jaydalvi@users.noreply.github.com>
2016-10-10 13:02:50 +0530

    Adding $D_Epoch to the event property example
    This allows users to trigger engagement at a date and time.

--- working_with_user_events/android/event_with_props.java
+++ working_with_user_events/android/event_with_props.java
@@ -3,6 +3,7 @@ HashMap<String, Object> prodViewedAction = new HashMap<String, Object>();
 prodViewedAction.put("Product Name", "Casio Chronograph Watch");
 prodViewedAction.put("Category", "Mens Accessories");
 prodViewedAction.put("Price", 59.99);
+prodViewedAction.put("Date", '$D_1476084698');
 
 cleverTap.event.push("Product viewed", prodViewedAction);
 
