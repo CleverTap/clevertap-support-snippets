e74a82c1d104f2b8f32b949df4fcb861e5f4bf85
Jude Pereira <judebpereira@gmail.com>
2016-10-18 15:47:56 +0530

    Update event_with_props.swift

--- working_with_user_events/ios/event_with_props.swift
+++ working_with_user_events/ios/event_with_props.swift
@@ -3,7 +3,7 @@ let props = [
     "Product name": "Casio Chronograph Watch",
     "Category": "Mens Accessories",
     "Price": 59.99,
-    "Date": "$D_1476084698"
+    "Date": NSDate()
 ]
 
 CleverTap.sharedInstance()?.recordEvent("Product viewed", withProps: props)
