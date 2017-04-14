1911b3f96ff55be1a1f3e8cdfc840d5b45b6df4a
Rohit Khanna <rohit121@gmail.com>
2017-04-14 14:50:30 +0530

    timezone added

--- working_with_user_profiles/ios/update_profile_basic.swift
+++ working_with_user_profiles/ios/update_profile_basic.swift
@@ -16,6 +16,8 @@ let profile: Dictionary<String, AnyObject> = [
     "Married": "Y",                         // Can be either Y or N
     "DOB": d!,                              // Date of Birth. An NSDate object
     "Age": 28,                              // Not required if DOB is set
+    "Tz":"Asia/Kolkata",                    //an abbreviation such as "PST", a full name such as "America/Los_Angeles", 
+                                            //or a custom ID such as "GMT-8:00"
     "Photo": "www.foobar.com/image.jpeg",   // URL to the Image
 
 // optional fields. controls whether the user will be sent email, push etc.
