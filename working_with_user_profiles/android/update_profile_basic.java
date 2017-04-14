61127331636d9a6841dd9865950bd9467130d85f
Rohit Khanna <rohit121@gmail.com>
2017-04-14 14:46:23 +0530

    timezone added

--- working_with_user_profiles/android/update_profile_basic.java
+++ working_with_user_profiles/android/update_profile_basic.java
@@ -11,6 +11,8 @@ profileUpdate.put("Education", "Graduate");                 // Can be either Gra
 profileUpdate.put("Married", "Y");                          // Can be either Y or N
 profileUpdate.put("DOB", new Date());                       // Date of Birth. Set the Date object to the appropriate value first
 profileUpdate.put("Age", 28);                               // Not required if DOB is set
+profileUpdate.put("Tz", "Asia/Kolkata");                    //an abbreviation such as "PST", a full name such as "America/Los_Angeles", 
+                                                            //or a custom ID such as "GMT-8:00"
 profileUpdate.put("Photo", "www.foobar.com/image.jpeg");    // URL to the Image
 
 // optional fields. controls whether the user will be sent email, push etc.
