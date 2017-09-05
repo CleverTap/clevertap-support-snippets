4a4b3495e0ebb285a116e28208f693238d3ba1e1
shahharsh093 <shah.harsh093@gmail.com>
2017-09-05 19:14:28 +0530

    Create unsubscription_payload.js

---
+++ email/unsubscription_payload.js
@@ -0,0 +1,9 @@
+{
+        "event": "unsubscribe",
+        "data": [
+            {
+                "e": "email5@emailprovider.com", // unsubscribed email id 
+                "ts": 1504619015
+            }
+        ]
+    }
