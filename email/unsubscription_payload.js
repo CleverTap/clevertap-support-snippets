d47800e85ac28ec1a6f3c324c57bc5f7149d8d81
shahharsh093 <shah.harsh093@gmail.com>
2017-09-05 23:10:52 +0530

    Update unsubscription_payload.js

--- email/unsubscription_payload.js
+++ email/unsubscription_payload.js
@@ -5,12 +5,12 @@
         "event": "unsubscribe",
         "data": [
             {
-                "e": "email1@emailprovider.com",  // email id that soft bounced
-                "ts": 1504632929                  // time of the bounce
+                "e": "email1@emailprovider.com",  // email id of the user
+                "ts": 1504632929                  // time of the unsubscribe
             },
             {
-                "e": "email2@emailprovider2.com", // email id that soft bounced
-                "ts": 1500630929                  // time of the bounce
+                "e": "email2@emailprovider2.com", // email id of the user
+                "ts": 1500630929                  // time of the unsubscribe
             }
         ]
     }
