5ec946be3e5bb9dbaa7bfc3142985922888dd587
Darshan Pania <darshan@clevertap.com>
2017-09-22 16:34:59 +0530

    Updating Log level example

--- getting_started/android/sdk_integration_debug_mode.java
+++ getting_started/android/sdk_integration_debug_mode.java
@@ -1,1 +1,5 @@
-CleverTapAPI.setDebugLevel(1);
+CleverTapAPI.setDebugLevel(CleverTap.Loglevel.INFO);    //Default Log level
+
+CleverTapAPI.setDebugLevel(CleverTap.Loglevel.DEBUG);   //Set Log level to DEBUG log warnings or other important messages
+
+CleverTapAPI.setDebugLevel(CleverTap.Loglevel.OFF);     //Switch off logs for Production environment
