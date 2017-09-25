35230cb3093fa973003a0d1ea1ff9587269d8067
Aditi3 <Aditi3@users.noreply.github.com>
2017-09-25 16:51:36 +0530

    Updated code for foreground notification

--- advance/ios/foreground_notification.swift
+++ advance/ios/foreground_notification.swift
@@ -1,23 +1,16 @@
- func userNotificationCenter(_ center: UNUserNotificationCenter,
-                                didReceive response: UNNotificationResponse,
-                                withCompletionHandler completionHandler: @escaping () -> Void) {
+func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
 
-        /**
-         Use this method to perform the tasks associated with your app's custom actions. When the user responds to a notification, the system calls this method with the results. You use this method to perform the task associated with that action, if at all. At the end of your implementation, you must call the completionHandler block to let the system know that you are done processing the notification.
+    /**
+    Use this method to perform the tasks associated with your app's custom actions. When the user responds to a notification, the system calls this method with the results. You use this method to perform the task associated with that action, if at all. At the end of your implementation, you must call the completionHandler block to let the system know that you are done processing the notification.
+    You specify your app's notification types and custom actions using UNNotificationCategory and UNNotificationAction objects. You create these objects at initialization time and register them with the user notification center. Even if you register custom actions, the action in the response parameter might indicate that the user dismissed the notification without performing any of your actions.
+    If you do not implement this method, your app never responds to custom actions.
+    see https://developer.apple.com/reference/usernotifications/unusernotificationcenterdelegate/1649501-usernotificationcenter
+    **/
 
-         You specify your app's notification types and custom actions using UNNotificationCategory and UNNotificationAction objects. You create these objects at initialization time and register them with the user notification center. Even if you register custom actions, the action in the response parameter might indicate that the user dismissed the notification without performing any of your actions.
+    print("APPDELEGATE: didReceiveResponseWithCompletionHandler \(response.notification.request.content.userInfo)")
 
-         If you do not implement this method, your app never responds to custom actions.
+    // if you wish CleverTap to record the notification click and fire any deep links contained in the payload
+    CleverTap.sharedInstance().handleNotification(withData: response.notification.request.content.userInfo, openDeepLinksInForeground: true)
 
-         see https://developer.apple.com/reference/usernotifications/unusernotificationcenterdelegate/1649501-usernotificationcenter
-
-         **/
-
-        print("APPDELEGATE: didReceiveResponseWithCompletionHandler \(response.notification.request.content.userInfo)")
-
-        // if you wish CleverTap to record the notification click and fire any deep links contained in the payload
-        CleverTap.sharedInstance().handleNotification(withData: response.notification.request.content.userInfo, openDeepLinksInForeground: true)
-
-        completionHandler()
-
-    } 
+    completionHandler()
+} 
