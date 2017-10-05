b208c4d7821655da5eaf69709f9f863707739d8e
Darshan Pania <darshan@clevertap.com>
2017-10-05 18:50:45 +0530

    Update sdk_integration_debug_mode.java

--- getting_started/android/sdk_integration_debug_mode.java
+++ getting_started/android/sdk_integration_debug_mode.java
@@ -1,5 +1,5 @@
-CleverTapAPI.setDebugLevel(CleverTap.Loglevel.INFO);    //Default Log level
+CleverTapAPI.setDebugLevel(CleverTapAPI.LogLevel.INFO);    //Default Log level
 
-CleverTapAPI.setDebugLevel(CleverTap.Loglevel.DEBUG);   //Set Log level to DEBUG log warnings or other important messages
+CleverTapAPI.setDebugLevel(CleverTapAPI.LogLevel.DEBUG);   //Set Log level to DEBUG log warnings or other important messages
 
-CleverTapAPI.setDebugLevel(CleverTap.Loglevel.OFF);     //Switch off logs for Production environment
+CleverTapAPI.setDebugLevel(CleverTapAPI.LogLevel.OFF);     //Switch off logs for Production environment
